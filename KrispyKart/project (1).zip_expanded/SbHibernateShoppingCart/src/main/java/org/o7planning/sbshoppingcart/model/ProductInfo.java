04720c546d6c44a60dc10f2a3985a2b40d193453
gagarao-melvin-216469637 <melving@my.yorku.ca>
2022-04-15 14:18:23 -0400

    Updated to V4

--- KrispyKart/project (1).zip_expanded/SbHibernateShoppingCart/src/main/java/org/o7planning/sbshoppingcart/model/ProductInfo.java
+++ KrispyKart/project (1).zip_expanded/SbHibernateShoppingCart/src/main/java/org/o7planning/sbshoppingcart/model/ProductInfo.java
@@ -4,7 +4,7 @@ import org.o7planning.sbshoppingcart.entity.Product;
 
 public class ProductInfo {
 	
-    private String code;
+    private String brand;
     private String name;
     private double price;
 
@@ -12,26 +12,26 @@ public class ProductInfo {
 
     // Create constructor to store code, name and price from product information
     public ProductInfo(Product product) {
-        this.code = product.getCode();
+        this.brand = product.getCode();
         this.name = product.getName();
         this.price = product.getPrice();
     }
 
     // Create constructor to store code, name and price
-    public ProductInfo(String code, String name, double price) {
-        this.code = code;
+    public ProductInfo(String brand, String name, double price) {
+        this.brand = brand;
         this.name = name;
         this.price = price;
     }
 
     // Returns product code
     public String getCode() {
-        return code;
+        return brand;
     }
 
     // Set product code
-    public void setCode(String code) {
-        this.code = code;
+    public void setCode(String brand) {
+        this.brand = brand;
     }
 
     // Returns product name
--- KrispyKart/project (1).zip_expanded/SbHibernateShoppingCart/src/main/java/org/o7planning/sbshoppingcart/utils/Utils.java
+++ KrispyKart/project (1).zip_expanded/SbHibernateShoppingCart/src/main/java/org/o7planning/sbshoppingcart/utils/Utils.java
@@ -6,37 +6,31 @@ import org.o7planning.sbshoppingcart.model.CartInfo;
 
 public class Utils {
 
-   // set the value of whatver is in the cart from the session
+   // Products in the cart, stored in Session.
    public static CartInfo getCartInSession(HttpServletRequest request) {
 
-      CartInfo cartInfo = (CartInfo) request.getSession().getAttribute("myCart"); //get the value of myCart from the session
+      CartInfo cartInfo = (CartInfo) request.getSession().getAttribute("myCart");
 
-      //if there is not information about the cart in the session, create a new cart and set the myCart attribute to the cart 
+   
       if (cartInfo == null) {
-    	  System.out.println("the cart info was null");
-      }
-      else {
-    	  cartInfo = new CartInfo();
-          
-          request.getSession().setAttribute("myCart", cartInfo); //set the session attribute for the cart info
+         cartInfo = new CartInfo();
+         
+         request.getSession().setAttribute("myCart", cartInfo);
       }
 
-      return cartInfo; //returns the information stored in the attribute myCart
+      return cartInfo;
    }
 
-  
-   //sets the lastOrderedCart attribute to be the latest cart information
+   public static void removeCartInSession(HttpServletRequest request) {
+      request.getSession().removeAttribute("myCart");
+   }
+
    public static void storeLastOrderedCartInSession(HttpServletRequest request, CartInfo cartInfo) {
       request.getSession().setAttribute("lastOrderedCart", cartInfo);
    }
-   
+
    public static CartInfo getLastOrderedCartInSession(HttpServletRequest request) {
       return (CartInfo) request.getSession().getAttribute("lastOrderedCart");
-   }
-   	
-   //removes myCart attribute from session
-   public static void removeCartInSession(HttpServletRequest request) {
-      request.getSession().removeAttribute("myCart");  
    }
    
 }
